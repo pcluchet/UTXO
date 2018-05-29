386b8bb471d7a693840ec6d55a4dbf64d05d246a
cactusfluo <cactus@ntymail.com>
2018-05-29 11:52:16 +0200

    [~]network/scripts/utils.sh: cleaned global function

--- network/scripts/utils.sh
+++ network/scripts/utils.sh
@@ -32,38 +32,6 @@ setGlobals () {
 	CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${ORG}.example.com/peers/peer0.org${ORG}.example.com/tls/ca.crt
 	CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${ORG}.example.com/users/Admin@org${ORG}.example.com/msp
 	CORE_PEER_ADDRESS=peer${PEER}.org${ORG}.example.com:7051
-	#if [ $ORG -eq 1 ] ; then
-	#	CORE_PEER_LOCALMSPID="Org1MSP"
-	#	CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-	#	CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-	#	if [ $PEER -eq 0 ]; then
-	#		CORE_PEER_ADDRESS=peer0.org1.example.com:7051
-	#	else
-	#		CORE_PEER_ADDRESS=peer1.org1.example.com:7051
-	#	fi
-	#elif [ $ORG -eq 2 ] ; then
-	#	CORE_PEER_LOCALMSPID="Org2MSP"
-	#	CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-	#	CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-	#	if [ $PEER -eq 0 ]; then
-	#		CORE_PEER_ADDRESS=peer0.org2.example.com:7051
-	#	else
-	#		CORE_PEER_ADDRESS=peer1.org2.example.com:7051
-	#	fi
-
-	#elif [ $ORG -eq 3 ] ; then
-	#	CORE_PEER_LOCALMSPID="Org3MSP"
-	#	CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
-	#	CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
-	#	if [ $PEER -eq 0 ]; then
-	#		CORE_PEER_ADDRESS=peer0.org3.example.com:7051
-	#	else
-	#		CORE_PEER_ADDRESS=peer1.org3.example.com:7051
-	#	fi
-	#else
-	#	echo "================== ERROR !!! ORG Unknown =================="
-	#fi
-
 	env |grep CORE
 }
 
